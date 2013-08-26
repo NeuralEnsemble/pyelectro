d1d404724dd34324b570f641dd723474e89a64e3
Mike Vella <vellamike@gmail.com>
2013-08-26 22:23:00 +0100

    adding some mocks to help autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -244,3 +244,10 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
