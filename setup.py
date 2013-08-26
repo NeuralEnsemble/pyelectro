61ca05b9652bcb5afa8917f973af39f04e482d75
Mike Vella <vellamike@gmail.com>
2013-08-26 21:43:08 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = "pyelectro",
+    version = '0.1.1dev',
+    packages = ['pyelectro'],
+    author = "Michael Vella",
+    author_email = "mv333@cam.ac.uk",
+    description = "A Python library for analysis of electrophysiological data",
+    license = "BSD",
+    url='https://github.com/vellamike/pyelectro',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Scientific/Engineering']
+)
