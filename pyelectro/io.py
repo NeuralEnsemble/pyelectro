5fe10e68513da333f4c3825b5740c333fa692e1e
Mike Vella <vellamike@gmail.com>
2013-12-31 15:36:32 +0000

    new IO module

--- pyelectro/io.py
+++ pyelectro/io.py
@@ -2,6 +2,9 @@
 IO operations, these are mostly specific to in-house formats used by Hugh Robinson's lab at Cambridge.
 """
 
+import scipy.io
+import numpy as np
+
 def data_from_sweep(path):
     """Extract the trace from a MATLAB sweep file
     
