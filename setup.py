f7653be37ea0f8f4ce17993e7ef29521c2952685
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-11-23 15:26:40 +0000

    feat: note python3 versions in setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,10 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 2.6',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         'Topic :: Scientific/Engineering']
 )
