b700aa5f16ddedbe2d6ce069c0a73e386e4fa1c6
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-06-14 15:38:09 +0100

    fix: correct package name in init

--- pyelectro/__init__.py
+++ pyelectro/__init__.py
@@ -5,7 +5,7 @@ Package for analysis of electrophysiology data in Python.
 
 try:
     import importlib.metadata
-    __version__ = importlib.metadata.version("neurotune")
+    __version__ = importlib.metadata.version("pyelectro")
 except ImportError:
     import importlib_metadata
-    __version__ = importlib_metadata.version("neurotune")
+    __version__ = importlib_metadata.version("pyelectro")
